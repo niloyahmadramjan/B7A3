e55d942b1706af629c43648f4d6716ae9594dbea
RAMJAN <niloyahmadramjan@gmail.com>
2026-06-15 01:50:28 +0800

    creaed matches table

--- QUERY.sql
+++ QUERY.sql
@@ -10,3 +10,14 @@ create table Users (
     role varchar(50) not null check( role in ('Ticket Manager', 'Football Fan')),
     phone_number varchar(20) null
 )
+
+-- create matches table 
+
+create table Matches(
+  match_id serial primary key,
+  fixture varchar(200) not null,
+  tournament_category varchar(100) not null,
+  base_ticket_price numeric(10,2) not null,
+  match_status varchar(20) not null
+  check(match_status IN ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
+)
