6c4c58f30f69ff591056f15233f484536ef0d4ee
RAMJAN <niloyahmadramjan@gmail.com>
2026-06-15 02:02:44 +0800

    created query for insert matches data

--- QUERY.sql
+++ QUERY.sql
@@ -40,3 +40,13 @@ insert into Users (user_id, full_name, email, role, phone_number) values
 (2, 'Asif Haque',    'asif@mail.com',   'Football Fan',   '+8801722222222'),
 (3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
 (4, 'Jannat Ara',    'jannat@mail.com', 'Football Fan',   NULL);
+
+
+-- created matches data insert query 
+
+insert into Matches(match_id, fixture, tournament_category, base_ticket_price, match_status) values
+(101, 'Real Madrid vs Barcelona',  'Champions League', 150, 'Available'),
+(102, 'Man City vs Liverpool',     'Premier League',   120, 'Selling Fast'),
+(103, 'Bayern Munich vs PSG',      'Champions League', 130, 'Available'),
+(104, 'AC Milan vs Inter Milan',   'Serie A',           90, 'Sold Out'),
+(105, 'Juventus vs Roma',          'Serie A',           80, 'Available');
